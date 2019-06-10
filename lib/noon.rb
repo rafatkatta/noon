d7117775123ca03947bf92b4322306425e7f5d23
Rafat Katta <rafatkatta@gsibc.net>
2019-06-10 20:42:53 +0200

    neen basic methods uptoken downtoken uuid val for neen base struct

--- lib/noon.rb
+++ lib/noon.rb
@@ -1,6 +1,60 @@
 require "noon/version"
+require 'securerandom'
+require 'digest'
+require 'time'
 
 module Noon
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Neen
+    attr_reader :neen, :val    
+    
+    @val = 2
+    
+    def initialize(value = 2)
+      @val = value if value >= 2
+      @neen = {uuid: uuid, uptoken: uptoken, downtoken: downtoken}         
+    end
+
+    def uptoken
+      Neen.uptoken
+    end
+
+    def downtoken
+      Neen.downtoken
+    end 
+
+    def uuid
+      Neen.uuid
+    end
+
+    private
+
+    def self.val(val)
+      @val = val
+    end
+
+    def self.uptoken
+      int = 1
+      begin	
+        int += 1
+        token = Digest::SHA256.hexdigest "بسم الله الواحد الأحد الجبار الفرد الصمد لا إله إلا هوخالق السموات و الأرض و الخلق أجمعين #{uuid} #{int}" 
+      end until token[0..@val] =~ /^0*$/
+      token
+    end
+
+    def self.downtoken
+      int = 1
+      begin
+        int += 1    
+        token = Digest::SHA512.hexdigest "لا غالب إلا الله له الملك الواحد القهار لا إله غلا هو الملك القدوس الرزاق السلام المؤمن المهيمن #{uuid} #{int}"
+      end until token[0..(@val+1)] =~ /^0*$/
+      token
+    end
+
+    def self.uuid
+      SecureRandom.uuid      
+    end
+  end
+
 end
--- spec/noon_spec.rb
+++ spec/noon_spec.rb
@@ -1,9 +1,58 @@
 RSpec.describe Noon do
+
   it "has a version number" do
+    puts Noon::VERSION
     expect(Noon::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "check neen uptoken" do
+    expect(Noon::Neen.uptoken).not_to be nil
   end
+
+  it "check neen downtoken" do
+    expect(Noon::Neen.downtoken).not_to be nil
+  end
+
+  it "check neen uuid" do
+    expect(Noon::Neen.uuid).not_to be nil
+  end
+
+  it "check neen uptoken diff true" do
+    uptokens = []
+    (0..5).each do |i|
+     uptokens << Noon::Neen.uptoken
+    end
+    expect(uptokens.uniq.size).to be uptokens.size
+  end
+
+  it "check neen downtoken diff true" do
+    downtokens = []
+    (0..5).each do |i|
+     downtokens << Noon::Neen.downtoken
+    end
+    expect(downtokens.uniq.size).to be downtokens.size
+  end
+
+  it "set value 3 at start check up and down tokens" do
+    Noon::Neen.val 3
+    uptoken = Noon::Neen.uptoken
+    downtoken = Noon::Neen.downtoken
+    expect(uptoken).to match(/\A0{4}/)
+    expect(downtoken).not_to match(/\A0{8}/)         
+  end
+  
+  it "check new neen with default value == 2" do
+     neen = Noon::Neen.new
+     expect(neen.val).to be 2
+     expect(neen.neen).not_to be nil
+  end
+
+  it "check new neen with default value == 3" do
+     neen = Noon::Neen.new 3
+     expect(neen.val).to be 3
+     expect(neen.neen).not_to be nil
+     expect(neen.uptoken).to match(/\A0{4}/)
+     expect(neen.downtoken).to match(/\A0{5}/)
+  end
+
 end
